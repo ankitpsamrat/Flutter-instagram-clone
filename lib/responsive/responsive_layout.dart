be5cd7811e5d51e552734ecb7ba78ef6fd2d121f
ankitpsamrat <ankit.p.samrat@gmail.com>
2022-07-11 20:33:30 +0530

    first commit

---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/utils/diamention.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({
+    Key? key,
+    required this.mobileScreenLayout,
+    required this.webScreenLayout,
+  }) : super(key: key);
+
+  final Widget mobileScreenLayout;
+  final Widget webScreenLayout;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: ((context, constraints) {
+        if (constraints.maxWidth > webScreenSize) {
+          return webScreenLayout;
+        }
+        return mobileScreenLayout;
+      }),
+    );
+  }
+}
